391b2df2a6b2e2ee861fa940463e7346f5dd4c8a
Ankit Singh <ankitsingh635101@gmail.com>
2025-09-06 12:47:54 +0530

    intial setup of project

---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "SnapShade – Beautiful Screenshots Instantly",
+  description:
+    "Turn plain screenshots into aesthetic posts with auto-generated gradients, padding, and shadows. Download or share directly to Twitter.",
+};
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
